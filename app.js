030bdab1c059016e562a4e6a676f5d38dd71a74b
Daniel Semel <dsemel@yahoo.com>
2025-03-31 16:31:57 -0700

    Updated app.js force redirect https

--- app.js
+++ app.js
@@ -47,6 +47,14 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production' && req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
